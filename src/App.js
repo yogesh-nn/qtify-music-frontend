3bf7f6e02eb30d95728a96534aa38e3b0f67218e
Nishchay Jain <nishchayjain.it20@jecrc.ac.in>
2024-11-26 13:35:26 +0530

    working qtify changes

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from "react";
+import Hero from "./components/Hero/Hero";
+import HomePage from "./pages/HomePage/HomePage";
+import Navbar from "./components/Navbar/Navbar";
+import StyledEngineProvider from "@mui/material/StyledEngineProvider";
+import { Outlet } from "react-router-dom";
+import {
+  fetchFilters,
+  fetchNewAlbums,
+  fetchSongs,
+  fetchTopAlbums,
+} from "./api/api";
+
+function App() {
+  const [data, setData] = useState({});
+
+  // const r = {
+  //   topAlbums: [{}, {}, {}, {}],
+  //    newAlbums: [{}, {}, {}, {}],
+  //    genres: ['rock', 'pop', 'jazz'],
+  //    songs: []
+  // };
+
+  const generateData = (key, source) => {
+    source().then((data) => {
+      setData((prevState) => {
+        // Object.assign would also work
+        return { ...prevState, [key]: data };
+      });
+    });
+  };
+
+  useEffect(() => {
+    generateData("topAlbums", fetchTopAlbums);
+    generateData("newAlbums", fetchNewAlbums);
+    generateData("songs", fetchSongs);
+    generateData("genres", fetchFilters);
+  }, []);
+
+  const { topAlbums = [], newAlbums = [], songs = [], genres = [] } = data;
+
+  return (
+    <>
+      <StyledEngineProvider injectFirst>
+        <Navbar searchData={[...topAlbums, ...newAlbums]} />
+        <Outlet context={{ data: { topAlbums, newAlbums, songs, genres } }} />
+      </StyledEngineProvider>
+    </>
+  );
+}
+
+// {data: {
+//   topAlbums: [],
+//   newAlbums: [],
+//   genres: [],
+//   songs: []
+// }}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
+import HomePage from "./pages/HomePage/HomePage";
+import "./index.css";
+import AlbumPage from "./pages/AlbumPage/AlbumPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/album/:albumId",
+        element: <AlbumPage />,
+      },
+    ],
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<RouterProvider router={router} />);
